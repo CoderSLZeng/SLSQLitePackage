946b182ec53aeea63b93fbb0e27b52960a911f86
CoderSLZeng <zeng@CoderSLZengdeMac-mini.local>
2017-10-19 18:08:05 +0800

    创建工程

---
+++ SLSQLitePackage/SLSQLitePackage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLSQLitePackage
+//
+//  Created by CoderSLZeng on 2017/10/19.
+//  Copyright © 2017年 GitHub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
